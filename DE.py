916b30482211d3c2ee9c9f9b346ca19fd3e098a7
SPRCSY <36022626+SPRCSY@users.noreply.github.com>
2018-08-21 19:44:08 +0800

    换了一个能跑的差分进化
    换了一个能跑的差分进化

--- DE.py
+++ DE.py
@@ -1,104 +1,84 @@
-import numpy as np
-import random as rd
-import math 
-
-pop_size = 300  # 种群数量
-G = 200 # 迭代次数
-D = 2   # 问题的维数
-F = 0.6  # 变异算子
-CR = 0.1  # 交换概率
-fitness = []  # 适应度
-fitnessg = []
-fitnessu = []
-fitness_mean =[]       # 平均适应度
-fitnessu_mean = [0]
-optimum_solution = []  # 每次迭代的所获得的最优解
-POP = []
-POPV = []
-POPVg=[]
-U=[]
-
-for i in range (pop_size):
-    fitnessg.append(0)
-    POPVg.append(0)
-    fitnessu.append(0)
-    U.append([0,0])
-rng=np.random
-
-
-POPV.append(rng.rand(pop_size,D))
-POP.append(rng.rand(pop_size,D))
-
-#定义适应度函数
-def fitnessf(x1,x2):
-    value = 3*math.cos(x1*x2)+x1+x2
-    return value
-    
-#定义适应度计算函数
-def calculate_fitness(g):
-    for j in range(pop_size):
-        y=POP[g]
-        x1=y[j][0]
-        x2=y[j][1]
-        fitnessg[j]=fitnessf(x1,x2)
-    fitness.append(fitnessg)
-    fitness_mean.append(sum(fitnessg)/pop_size)
-
-def calculate_fitnessu():
-    for j in range (pop_size):
-        x1=U[j][0]
-        x2=U[j][1]
-        fitnessu[j]=fitnessf(x1,x2)
-        fitnessu_mean[0]=sum(fitnessu)/pop_size
-        
-def mutation(g):
-    for i in range(pop_size):
-        r=[0,0,0]
-        r[0]=rd.randint(0,pop_size-1)
-        r[1]=rd.randint(0,pop_size-1)
-        r[2]=rd.randint(0,pop_size-1)
-        POPVg[i]=X[r[0]]+F*(X[r[1]]-X[r[2]])
-        POPV.append(POPVg)
-
-def rn(i):
-    return rd.randint(0,D-1)
-
-def crossover():
-    rc=0
-    for i in range (pop_size):
-        rc=rd.random()
-        if (rc<=CR or rn(i)==0):
-            U[i][0]=POPVg[i][0]
-            U[i][1]=X[i][1]
-        else:
-            U[i][0]=X[i][1]
-            U[i][1]=POPVg[i][0]
-            
-def choose(g):
-    for i in range (pop_size):
-        if fitnessu[i]>fitness[g][i]:
-            X[i]=U[i]
-        else:
-            X[i]=X[i]
-    POP.append(X)       
-            
-       
+# -*- coding: utf-8 -*- 
+import numpy as np 
+import random 
+from math import cos 
+ 
+pop_size = 500  # 种群数量 
+G = 3000  # 迭代次数 
+D = 2   # 问题的维数 
+F = 0.5  # 变异算子 
+CR = 0.5  # 交换概率 
+fitness = np.zeros(pop_size)  # 适应度 
+fitness_best=[]
+fitness_mean = [] #本代平均适应度 
 g=0
-while (g<=G):
-    calculate_fitness(g)#计算适应值 
-    X=POP[g]
-    j=0
-    maxc=0
-    for j in range (pop_size):
-        if (fitness[g][j]>=max(fitness[g])):
-            maxc=j
-        optimum_solution.append(POP[g][maxc])
-    if (fitness[g][maxc]-fitness_mean[g]<=0.1):  #终止条件
-        break
-    else:
-        mutation(g)
-        crossover()
-        calculate_fitnessu()
-        choose(g)
-        g=g+1
-print ("x=%p\n",optimum_solution[g])        
+P_best = []  # 每次迭代的所获得的最优解 
+P = []   #原始种群
+V = np.zeros((pop_size,D),dtype=np.float64)  #差分进化后得到的临时种群
+U = np.zeros((pop_size,D),dtype=np.float64) #V变异后的临时种群
+    
+P.append(np.random.rand(pop_size,D)) #生成一个初始种群P【0】
+ 
+#定义适应度函数 
+def fitnessf(X): 
+    x=X[0]
+    y=X[1]
+    value =  (1*cos((1+1)*x+1)+2*cos((2+1)*x+2)+3*cos((3+1)*x+3)+4*cos((4+1)*x+4)+5*cos((5+1)*x+5))*(1*cos((1+1)*y+1)+2*cos((2+1)*y+2)+3*cos((3+1)*y+3)+4*cos((4+1)*y+4)+5*cos((5+1)*y+5))
+    return value 
+     
+#定义适应度计算函数 
+def calculate_fitness(): 
+    global fitness
+    for j in range(pop_size): 
+        y=P[g] 
+        fitness[j]=fitnessf(y[j]) 
+    global fitness_mean
+    fitness_mean.append(sum(fitness)/pop_size)
+    j=0 
+    maxc=0 
+    for j in range (pop_size): 
+        if (fitness[j]==max(fitness)): 
+            maxc=j 
+    global P_best
+    global fitness_best 
+    P_best.append(P[g][maxc]) 
+    fitness_best.append(max(fitness))
+    
+def mutation(X):
+    global V
+    for i in range (pop_size):
+        r1=random.randint(0,pop_size-1)
+        r2=r1
+        while (r2==r1):
+            r2=random.randint(0,pop_size-1)
+        V[i]=X[i]+F*(X[r1]-X[r2]) 
+            #差分变异
+    
+def crossover():    #交叉互换
+    global U
+    for i in range (pop_size):
+        if (random.random()<=CR):
+            r=random.randint(0,D-1)
+            U[i]=V[i]
+            for j in range (r,D-1):
+                U[i][j]=P[g][i][j]
+        
+def select():   #选择操作
+    W = U
+    global P
+    for i in range (pop_size):
+        if (fitnessf(W[i])<fitness[i]):
+            W[i]=P[g][i]
+    P.append(W)        
+
+calculate_fitness()#计算适应值
+while (g<=G):     
+    print (g ,"x=",P_best[g],fitness_best[g]) 
+    mutation(P[g])
+    crossover()
+    select()
+    g=g+1
+    calculate_fitness() 
+    
+print ("最优解为：x=",P_best[fitness_best.index(max(fitness_best))])
+print ("此时适应度为：z=",max(fitness_best))    
